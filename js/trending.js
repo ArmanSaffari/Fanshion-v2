c225ded7ec530af91f9ab479d7cdcb5c820b1e04
ArmanSaffari <a.saffari256@gmail.com>
2023-06-09 12:06:24 -0400

    grabbed news added to carousel

--- js/trending.js
+++ js/trending.js
@@ -1,51 +1,108 @@
 window.addEventListener("DOMContentLoaded", async function () {
 
-async function addSlideToCarousel(imageUrl, title, content) {
-  const carouselInner = document.querySelector("#newsCarousel .carousel-inner");
-  const carouselIndicators = document.querySelector("#newsCarousel .carousel-indicators");
+   const newsImages = [
+  "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+   "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+   "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+   "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+   "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+   "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+   "https://source.unsplash.com/68csPWTnafo",
+   "https://source.unsplash.com/HY1fq4ZtLTE",
+   "https://source.unsplash.com/kfSjyxAWhEc",
+   "https://source.unsplash.com/f7xoQpzl-Mo",
+   "https://source.unsplash.com/dWhsNvhfC8c",
+  ]
 
-  // Create carousel item
-  const carouselItem = document.createElement("div");
-  carouselItem.classList.add("carousel-item");
+  getNews();
 
-  // Create image element
-  // const image = document.createElement("img");
-  // image.classList.add("d-block", "w-100", "object-fit-cover");
-  // image.setAttribute("height", "500px");
-  // image.src = imageUrl;
-  // image.alt = "Slide";
-
-  // Append the image to the carousel item
-  // carouselItem.appendChild(image);
-  carouselItem.innerHTML = `
-    <img class="d-block w-100 object-fit-cover" height="500px" src=${imageUrl} alt="..." />
-    <div class="carousel-caption d-none d-md-block">
-      <h3>${title}</h3>
-      <p>${content}</p>
-    </div>`
-
-  // Append the carousel item to the carousel inner
-  carouselInner.appendChild(carouselItem);
-
-  // Create carousel indicator
-  const carouselIndicator = document.createElement("button");
-  carouselIndicator.setAttribute("type", "button");
-  carouselIndicator.setAttribute("data-bs-target", "#newsCarousel");
-  carouselIndicator.setAttribute("data-bs-slide-to", carouselInner.childElementCount - 1);
-
-  // Add active class to the first indicator
-  if (carouselInner.childElementCount === 1) {
-    carouselIndicator.classList.add("active");
+  async function getNews() {
+    const url = 'https://fashion-industry-news-data-collection.p.rapidapi.com/?q=%22fashion%20week%22&lang="en"';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'a3e84bd330msheda336f89d90189p100b74jsn157ced0ce4ed',
+        'X-RapidAPI-Host': 'fashion-industry-news-data-collection.p.rapidapi.com'
+      }
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      const data = JSON.parse(result).response.data;
+      const englishContent = data.filter(article => article.language == 'en');
+      await displayNews(englishContent);
+  
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  // Append the indicator to the carousel indicators
-  carouselIndicators.appendChild(carouselIndicator);
-}
+  async function displayNews(news) {
 
-// Usage:
+    console.log(news);
 
-const imageUrl = "../assets/news/news1.jpg"; // Replace with the actual image URL
-addSlideToCarousel(imageUrl, "SECOND", "some description");
-// removeFirst()
+    await news.forEach((item, index) => {
+      addSlideToCarousel(item.title, item.url, newsImages[index])
+    });
+
+  };
+
+  async function addSlideToCarousel(title, url, imageUrl) {
+    const carouselInner = document.querySelector("#newsCarousel .carousel-inner");
+    const carouselIndicators = document.querySelector("#newsCarousel .carousel-indicators");
+
+    // Create carousel item
+    const carouselItem = document.createElement("div");
+    carouselItem.classList.add("carousel-item");
+
+    carouselItem.innerHTML = `
+      <img class="d-block w-100 object-fit-cover" height="500px" src=${imageUrl} alt="..." />
+      <div class="bg-white bg-opacity-50 rounded-3 carousel-caption">
+        <h5 class="mb-4 text-dark">${title}</h5>
+        <button href=${url} type="button" class="btn btn-outline-dark">Read More</button>
+      </div>`
+
+    // Append the carousel item to the carousel inner
+    carouselInner.appendChild(carouselItem);
+
+    // Create carousel indicator
+    const carouselIndicator = document.createElement("button");
+    carouselIndicator.setAttribute("type", "button");
+    carouselIndicator.setAttribute("data-bs-target", "#newsCarousel");
+    carouselIndicator.setAttribute("data-bs-slide-to", carouselInner.childElementCount - 1);
+
+    // Add active class to the first indicator
+    if (carouselInner.childElementCount === 1) {
+      carouselIndicator.classList.add("active");
+    }
+
+    // Append the indicator to the carousel indicators
+    carouselIndicators.appendChild(carouselIndicator);
+  }
 
 });
